e006c26598f908ed122ed0ce1e1154969bc8a121
seongwon-ha <djfqks22@naver.com>
2023-02-10 12:47:02 +0900

    리스트 5

---
+++ src/chap09/DblLinkedList.java
@@ -0,0 +1,31 @@
+package chap09;
+
+public class DblLinkedList<E> {
+    class Node<E> {
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node() {
+            data = null;
+            prev = next = this;
+        }
+
+        Node(E obj, Node<E> prev, Node<E> next) {
+            data = obj;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> crnt;
+
+    public DblLinkedList() {
+        head = crnt = new Node<E>();
+    }
+
+    public boolean isEmpty() {
+        return head.next == head;
+    }
+}
