30de3cb3dba012a4c5f0c1ef8983e84327d6819b
seongwon-ha <djfqks22@naver.com>
2023-03-04 22:47:28 +0900

    해쉬 6

--- src/chap11/OpenHash.java
+++ src/chap11/OpenHash.java
@@ -21,7 +21,40 @@ public class OpenHash<K, V> {
         void setStat(Status stat) {
             this.stat = stat;
         }
+
+        K getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return data;
+        }
+
+        public int hashCode() {
+            return key.hashCode();
+        }
     }
 
+    private int size;
+    private Bucket<K, V>[] table;
+
+    public OpenHash(int size) {
+        try {
+            table = new Bucket[size];
+            for(int i = 0; i < size; i++)
+                table[i] = new Bucket<K, V>();
+            this.size = size;
+        } catch (OutOfMemoryError e) {
+            this.size = 0;
+        }
+    }
+
+    public int hashValue(Object key) {
+        return key.hashCode() % size;
+    }
+
+    public int rehashValue(int hash) {
+        return (hash+1) % size;
+    }
 
 }
