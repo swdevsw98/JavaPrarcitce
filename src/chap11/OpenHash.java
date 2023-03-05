915a94ec0c5ee7e7a53ab5984a89f99652c1588d
seongwon-ha <djfqks22@naver.com>
2023-03-05 22:58:57 +0900

    해쉬 7

--- src/chap11/OpenHash.java
+++ src/chap11/OpenHash.java
@@ -57,4 +57,70 @@ public class OpenHash<K, V> {
         return (hash+1) % size;
     }
 
+    private Bucket<K,V> searchNode(K key) {
+        int hash = hashValue(key);
+        Bucket<K, V> p = table[hash];
+
+        for(int i = 0; p.stat != Status.EMPTY && i < size; i++) {
+            if(p.stat == Status.OCCUPIED && p.getKey().equals(key))
+                return p;
+            hash = rehashValue(hash);
+            p = table[hash];
+        }
+        return null;
+    }
+
+    public V search(K key) {
+        Bucket<K, V> p = searchNode(key);
+        if(p != null)
+            return p.getValue();
+        else
+            return null;
+    }
+
+    public int add(K key, V data) {
+        if(search(key) != null)
+            return 1;
+
+        int hash = hashValue(key);
+        Bucket<K,V> p = table[hash];
+        for(int i = 0; i < size; i++){
+            if(p.stat == Status.EMPTY || p.stat == Status.DELETED) {
+                p.set(key, data, Status.OCCUPIED);
+                return 0;
+            }
+            hash = rehashValue(hash);
+            p = table[hash];
+        }
+        return 2;
+    }
+
+    public int remove(K key) {
+        Bucket<K,V> p = searchNode(key);
+        if(p==null)
+            return 1;
+
+        p.setStat(Status.DELETED);
+        return 0;
+    }
+
+    public void dump() {
+        for(int i = 0; i < size; i++) {
+            System.out.printf("%02d " , i);
+            switch(table[i].stat) {
+                case OCCUPIED:
+                    System.out.printf("%s (%s)\n",
+                            table[i].getKey(), table[i].getValue());
+                    break;
+
+                case EMPTY:
+                    System.out.println("-- 미등록 --");
+                    break;
+
+                case DELETED:
+                    System.out.println("-- 삭제 마침 --");
+                    break;
+            }
+        }
+    }
 }
